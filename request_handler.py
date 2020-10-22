34486c245bcfedfd41a02a69f00b048789f688f9
travisbrigman <travis1000@gmail.com>
2020-10-22 11:02:07 -0500

    category get and post working

--- request_handler.py
+++ request_handler.py
@@ -2,10 +2,19 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from models import Category, Post, Reaction, Tag
 from categories import get_categories, create_category
-from posts import get_all_posts, create_post, get_posts_by_category_id, get_posts_by_subscription, get_posts_by_user_id, get_single_post
+from posts import get_all_posts, create_post, get_posts_by_user_id, get_single_post
 from reactions import get_reactions, get_reactions_by_post_id, create_reaction
 from subscriptions import get_subscriptions, create_subscription
 from tags import get_tags, create_tag
+
+# This function is not inside the class. It is the starting
+# point of this application.
+def main():
+        host = ''
+        port = 8088
+        HTTPServer((host, port), HandleRequests).serve_forever()
+        if __name__ == "__main__":
+            main()
 
 class HandleRequests(BaseHTTPRequestHandler):
 
@@ -23,13 +32,80 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
 
-# This function is not inside the class. It is the starting
-# point of this application.
-def main():
-    host = ''
-    port = 8088
-    HTTPServer((host, port), HandleRequests).serve_forever()
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
 
-if __name__ == "__main__":
-    main()
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /animals
+            except ValueError:
+                pass  # Request had trailing slash: /animals/
+
+            return (resource, id)
+
+    def do_GET(self):
+        self._set_headers(200)
+
+        response = {}
+
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
+
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/animals` or `/animals/2`
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
+
+            # if resource == "entries":
+            #     if id is not None:
+            #         response = f"{get_single_entry(id)}"
+            #     else:
+            #         response = f"{get_all_entries()}"
+
+            if resource == "categories":
+                    response = f"{get_categories()}"
+
+        elif len(parsed) == 3:
+            ( resource, key, value ) = parsed
+
+        self.wfile.write(response.encode())
+
+
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new resource
+        new_resource = None
+
+        if resource == "categories":
+            new_resource = create_category(post_body)
+
+        # Encode the new resource and send in response
+        self.wfile.write(f"{new_resource}".encode())
