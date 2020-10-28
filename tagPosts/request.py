e8dc7014935e838a313eacc025b8cc9d3b5b7bff
justha <nganha42@gmail.com>
2020-10-28 09:24:42 -0500

    joined TagPost with Post and User tables

--- tagPosts/request.py
+++ tagPosts/request.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import TagPost, Post 
+from models import TagPost, Post, User, Category, Tag
 
 
 def get_tagPosts():
@@ -46,8 +46,16 @@ def get_tagPosts_by_tag_id(tag_id):
         SELECT
            tp.id,
            tp.tag_id,
-           tp.post_id
-        FROM tagPost tp 
+           tp.post_id,
+           t.tag,
+           p.title, 
+           u.display_name, 
+           c.type
+        FROM TagPost tp 
+        JOIN Tag t ON t.id = tp.tag_id
+        JOIN Post p ON p.id = tp.post_id
+        JOIN User u ON u.id = p.user_id
+        JOIN Category c ON c.id = p.category_id
         WHERE tp.tag_id = ?
         """, ( tag_id, ))
 
@@ -56,8 +64,20 @@ def get_tagPosts_by_tag_id(tag_id):
         dataset = db_cursor.fetchall()
 
         for row in dataset:
+
             tagPost = TagPost(row['id'], row['tag_id'], row['post_id'])
+
+            tag = Tag("", row['tag'])
+            post = Post("", row['title'], "", "", "", "", "")
+            user = User("", "", "", "", row['display_name'], "", "", "", "")
+            category = Category("", row['type'])
+
+            tagPost.tag = tag.__dict__
+            tagPost.post = post.__dict__
+            tagPost.user = user.__dict__
+            tagPost.category = category.__dict__
             tagPosts.append(tagPost.__dict__)
+
 
     return json.dumps(tagPosts)
 
